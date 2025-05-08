9da1753af8aca0d41940d1fe94d18864d30f0fe5
abhipsabasu <abhipsabasu@gmail.com>
2025-05-08 20:22:24 +0530

    small changes

--- streamlit_CEVI_China.py
+++ streamlit_CEVI_China.py
@@ -43,6 +43,38 @@ country = 'China'
 response = requests.get(GITHUB + f'{country}.csv')
 df = pd.read_csv(StringIO(response.text))
 
+IMAGE_LIST =  list(df['new_image_path'])# filenames in the GitHub repo
+QUESTIONS = {}
+ENTITY_LIST = list(df['concept'])
+
+q1 = "Rate this image on the level of *affluence*. The options are a scale of 1 to 5, where each score is defined within the options."
+q2 = "Rate this image on the *general condition* of the primary entity. The options are a scale of 1 to 5, where each score is defined within the options."
+q3 = "Rate this image on the *cultural localization* of the primary entity with respect to *your* country. The options are a scale of 1 to 5, where each score is defined within the options."
+
+a1 = ["1 – **Impoverished**:	Severe visible decay; disrepair, dirt, broken infrastructure, minimal economic activity.",
+      "2 – **Low Affluence**:	Basic but aging structures; modest upkeep; informal or patchy development visible.",
+      "3 – **Moderate Affluence**:	Clean and functional spaces; organized but simple environments; middle-income indicators.",
+      "4 – **High Affluence**:	Well-maintained, vibrant areas; professional storefronts; signs of prosperity and civic care."
+      "5 – **Very High Affluence (Luxury)**:	Sleek, modern, or designer elements; upscale brands; spotless, elite environments."]
+
+a2 = ["1 – **Severely Damaged**:	Major disrepair, heavy rust, breakage, or abandonment visible.",
+      "2 – **Poor Condition**:	Noticeable wear, aging, dirt, minor missing parts, but still recognizable.",
+      "3 – **Moderately Maintained**:	Functional, intact, but with small flaws like scuffs or fading.",
+      "4 – **Well Maintained**:	Clean, organized, minor cosmetic wear only, no functional damage.",
+      "5 – **Excellent Condition**:	Polished, pristine, flawless; appears new or recently serviced."]
+
+a3 = ["1 – **Highly globalized**: The subject displays no distinct cultural markers and appears universally generic or global in design.",
+      "2 – **Slightly localized**: The subject shows minor cultural hints, but these are subtle and easily overshadowed by global aesthetics.",
+      "3 – **Moderately localized**: The subject blends global and cultural elements, suggesting a recognizable yet not dominant cultural identity.",
+      "4 – **Strongly localized**: The subject prominently features distinctive cultural elements that are clearly tied to the local context.",
+      "5 – **Deeply rooted in culture**: The subject embodies cultural uniqueness through highly characteristic and tradition-rich visual cues."]
+
+for idx, row in df.iterrows():
+    QUESTIONS[row['new_image_path']] = [{"question": '**'+q1+'**', 
+                                    "options": a1},
+                                  {"question": '**'+q2+'**', 
+                                    "options": a2},
+
 if "page_idx" not in st.session_state:
     st.session_state.page_idx = 0
 if "responses" not in st.session_state:
